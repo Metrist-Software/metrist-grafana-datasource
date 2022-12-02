fe4f0859e440c01b9e2ff8494fd9c3f65a19bd31
aydanjiwanimetrist <aydan.jiwani@metrist.io>
2022-12-02 15:20:51 -0500

    initial commit, http stuff added

--- pkg/plugin/datasource.go
+++ pkg/plugin/datasource.go
@@ -3,10 +3,14 @@ package plugin
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"math/rand"
+	"net/http"
 	"time"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
@@ -23,20 +27,43 @@ var (
 	_ instancemgmt.InstanceDisposer = (*Datasource)(nil)
 )
 
+
+var (
+	errRemoteRequest  = errors.New("remote request error")
+	errRemoteResponse = errors.New("remote response error")
+)
+
 // NewDatasource creates a new datasource instance.
-func NewDatasource(_ backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
-	return &Datasource{}, nil
+func NewDatasource(settings backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+	opts, err := settings.HTTPClientOptions()
+	if err != nil {
+		return nil, fmt.Errorf("http client options: %w", err)
+	}
+	cl, err := httpclient.New(opts)
+	if err != nil {
+		return nil, fmt.Errorf("httpclient new: %w", err)
+	}
+	return &Datasource{
+		settings:   settings,
+		httpClient: cl,
+	}, nil
 }
 
 // Datasource is an example datasource which can respond to data queries, reports
 // its health and has streaming skills.
-type Datasource struct{}
+type Datasource struct{
+	settings backend.DataSourceInstanceSettings
+
+	httpClient *http.Client
+
+}
 
 // Dispose here tells plugin SDK that plugin wants to clean up resources when a new instance
 // created. As soon as datasource settings change detected by SDK old datasource instance will
 // be disposed and a new one will be created using NewSampleDatasource factory function.
 func (d *Datasource) Dispose() {
 	// Clean up datasource instance resources.
+	d.httpClient.CloseIdleConnections()
 }
 
 // QueryData handles multiple queries and returns multiple responses.
@@ -53,7 +80,7 @@ func (d *Datasource) QueryData(ctx context.Context, req *backend.QueryDataReques
 
 	// loop over queries and execute them individually.
 	for _, q := range req.Queries {
-		res := d.query(ctx, req.PluginContext, q)
+		res, _ := d.query(ctx, req.PluginContext, q)
 
 		// save the response in a hashmap
 		// based on with RefID as identifier
@@ -65,15 +92,44 @@ func (d *Datasource) QueryData(ctx context.Context, req *backend.QueryDataReques
 
 type queryModel struct{}
 
-func (d *Datasource) query(_ context.Context, pCtx backend.PluginContext, query backend.DataQuery) backend.DataResponse {
-	var response backend.DataResponse
+func (d *Datasource) query(ctx context.Context, pCtx backend.PluginContext, query backend.DataQuery) (backend.DataResponse, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, d.settings.URL, nil)
+	req.Header.Add("auth", pCtx.DataSourceInstanceSettings.DecryptedSecureJSONData["apikey"])
+	if err != nil {
+		return backend.DataResponse{}, fmt.Errorf("new request with context: %w", err)
+	}
+	resp, err := d.httpClient.Do(req)
+	switch {
+	case err == nil:
+		break
+	case errors.Is(err, context.DeadlineExceeded):
+		return backend.DataResponse{}, err
+	default:
+		return backend.DataResponse{}, fmt.Errorf("http client do: %w: %s", errRemoteRequest, err)
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.DefaultLogger.Error("query: failed to close response body", "err", err)
+		}
+	}()
+
+	// Make sure the response was successful
+	if resp.StatusCode != http.StatusOK {
+		return backend.DataResponse{}, fmt.Errorf("%w: expected 200 response, got %d", errRemoteResponse, resp.StatusCode)
+	}
+
+	// Decode response
+	var body apiMetrics
+	if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
+		return backend.DataResponse{}, fmt.Errorf("%w: decode: %s", errRemoteRequest, err)
+	}
 
 	// Unmarshal the JSON into our queryModel.
 	var qm queryModel
 
-	err := json.Unmarshal(query.JSON, &qm)
-	if err != nil {
-		return backend.ErrDataResponse(backend.StatusBadRequest, "json unmarshal: "+err.Error())
+	jsonerr := json.Unmarshal(query.JSON, &qm)
+	if jsonerr != nil {
+		return backend.ErrDataResponse(backend.StatusBadRequest, "json unmarshal: "+jsonerr.Error()), jsonerr
 	}
 
 	// create data frame response.
@@ -86,9 +142,9 @@ func (d *Datasource) query(_ context.Context, pCtx backend.PluginContext, query 
 	)
 
 	// add the frames to the response.
-	response.Frames = append(response.Frames, frame)
-
-	return response
+	return backend.DataResponse{
+		Frames: []*data.Frame{ frame },
+	}, nil
 }
 
 // CheckHealth handles health checks sent from Grafana to the plugin.
