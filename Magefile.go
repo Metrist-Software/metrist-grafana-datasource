9cd97df9fb0695d058e32db88a54853bd96a6eee
Daven Casia <daven@metrist.io>
2023-01-24 19:48:15 +0000

    Add env in mage

--- Magefile.go
+++ Magefile.go
@@ -4,9 +4,26 @@
 package main
 
 import (
+	"os"
+
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
+
+func init() {
+	// use the default if not set
+	environment, ok := os.LookupEnv("ENV")
+	if !ok {
+		environment = "dev"
+	}
+	build.SetBeforeBuildCallback(func(cfg build.Config) (build.Config, error) {
+		cfg.CustomVars = map[string]string{
+			"internal.Environment": environment,
+		}
+
+		return cfg, nil
+	})
+}
 
 // Default configures the default target.
 var Default = build.BuildAll
