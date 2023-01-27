ca6360194cbc3fd14cd2ed333e7b15733349f6ac
Daven Casia <daven@metrist.io>
2023-01-27 18:00:32 +0000

    Propagate errors

--- Magefile.go
+++ Magefile.go
@@ -7,60 +7,64 @@ import (
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 
-	"fmt"
 	"os"
 
 	"github.com/magefile/mage/sh"
 )
 
 // Generate a production build (pointing to the production API)
-func BuildProd() {
-	buildForEnv("prod")
+func BuildProd() error {
+	return buildForEnv("prod")
 }
 
 // Generate a dev build (pointing to the dev API)
-func BuildDev() {
-	buildForEnv("dev")
+func BuildDev() error {
+	return buildForEnv("dev")
 }
 
 // Generate a dev or production built dependent on the GITHUB_REF env var (defaults to dev)
-func Build() {
+func Build() error {
 	buildFunction := getBuildFunc()
-	buildFunction()
+	return buildFunction()
 }
 
 // Default configures the default target.
 var Default = Build
 
 // Helper functions
-func getHash() string {
-	if hash, err := sh.Output("git", "rev-parse", "--short", "HEAD"); err != nil {
-		fmt.Println(err)
-		return ""
-	} else {
-		return hash
-	}
+func getHash() (hash string, err error) {
+	hash, err = sh.Output("git", "rev-parse", "--short", "HEAD")
+	return
 }
 
-func buildForEnv(env string) {
+func buildForEnv(env string) error {
+	buildHash, err := getHash()
+
+	if err != nil {
+		return err
+	}
+
 	// We are using Grafana's build functions so if we want custom ldflag values we have to hook here
-	build.SetBeforeBuildCallback(func(cfg build.Config) (build.Config, error) {
+	if err := build.SetBeforeBuildCallback(func(cfg build.Config) (build.Config, error) {
 		cfg.CustomVars = map[string]string{
 			"github.com/Metrist-Software/metrist-grafana-datasource/pkg/internal.Environment": env,
-			"github.com/Metrist-Software/metrist-grafana-datasource/pkg/internal.BuildHash":   getHash(),
+			"github.com/Metrist-Software/metrist-grafana-datasource/pkg/internal.BuildHash":   buildHash,
 		}
 
 		return cfg, nil
-	})
+	}); err != nil {
+		return err
+	}
 
 	build.BuildAll()
+	return nil
 }
 
-func getBuildFunc() func() {
+func getBuildFunc() func() error {
 	environment, _ := os.LookupEnv("GITHUB_REF")
 	if environment == "refs/heads/main" {
 		return BuildProd
-	} else {
-		return BuildDev
 	}
+
+	return BuildDev
 }
