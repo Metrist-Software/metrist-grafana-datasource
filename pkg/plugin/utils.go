ded3f0df682432d9913b2622076b26750b43868c
Daven Casia <daven@metrist.io>
2023-02-07 18:31:42 +0000

    Add helper

---
+++ pkg/plugin/utils.go
@@ -0,0 +1,16 @@
+package plugin
+
+func uniqStrings(xs []string) []string {
+	set := make(map[string]bool)
+	for _, x := range xs {
+		if _, ok := set[x]; !ok {
+			set[x] = true
+		}
+	}
+
+	res := []string{}
+	for k := range set {
+		res = append(res, k)
+	}
+	return res
+}
